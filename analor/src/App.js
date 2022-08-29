449f5684ac1fa44824a79015f40c6aa172fa6ece
luiz <luizhenriquetrovao9@gmail.com>
2022-08-29 13:50:40 -0300

    adcionado-forms-de-propriedades

--- analor/src/App.js
+++ analor/src/App.js
@@ -1,10 +1,13 @@
 import Logo from './components/Logo.js';
 import Elementos from './components/Elementos.js';
+import Propriedades from './components/Propriedades.js';
+
 export default function App() {
   return (
     <>
       <Logo />
       <Elementos />
+      <Propriedades />
     </>
   );
 }
