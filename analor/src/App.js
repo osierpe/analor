41bd1c20b8dcc20820959356518f4e3e7580824f
luiz <luizhenriquetrovao9@gmail.com>
2022-09-07 08:23:15 -0300

    adcionado cas

--- analor/src/App.js
+++ analor/src/App.js
@@ -2,7 +2,7 @@ import Logo from './components/Logo.js';
 import Elementos from './components/Elementos.js';
 import Propriedades from './components/Propriedades.js';
 import GrupoFuncEsqCar from './components/GrupoFuncEsqCar.js';
-
+import Cas from './components/Cas.js';
 export default function App() {
   return (
     <>
@@ -10,6 +10,7 @@ export default function App() {
       <Elementos />
       <Propriedades />
       <GrupoFuncEsqCar />
+      <Cas />
     </>
   );
 }
