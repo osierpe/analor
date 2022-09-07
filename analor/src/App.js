280f916094fc276ab48890c718917ae76a8ec149
luiz <luizhenriquetrovao9@gmail.com>
2022-09-07 11:23:01 -0300

    adcionado butoes

--- analor/src/App.js
+++ analor/src/App.js
@@ -3,6 +3,8 @@ import Elementos from './components/Elementos.js';
 import Propriedades from './components/Propriedades.js';
 import GrupoFuncEsqCar from './components/GrupoFuncEsqCar.js';
 import Cas from './components/Cas.js';
+import Buttons from './components/Buttons.js';
+
 export default function App() {
   return (
     <form method='post'>
@@ -11,6 +13,7 @@ export default function App() {
       <Propriedades />
       <GrupoFuncEsqCar />
       <Cas />
+      <Buttons />
     </form>
   );
 }
---
+++ analor/src/components/Buttons.js
@@ -0,0 +1,10 @@
+export default function Buttons() {
+  return (
+    <div className='btns'>
+      <a href='/ref.php' class='button' target='_blank'>
+        Referencia Grup Func & Esq Carbono
+      </a>
+      <input type='submit' value='Aplicar filtro' class='envia'></input>
+    </div>
+  );
+}
