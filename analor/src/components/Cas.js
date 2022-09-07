cdf6716ae2805b6d2591156c788446f2c7fe0a27
luiz <luizhenriquetrovao9@gmail.com>
2022-09-07 08:23:15 -0300

    adcionado cas

---
+++ analor/src/components/Cas.js
@@ -0,0 +1,9 @@
+export default function Cas() {
+  return (
+    <div className='cas'>
+      <h2>CAS</h2>
+      <label htmlfor='scales'>CAS: </label>
+      <input type='text' name='cas' maxlength='15' />
+    </div>
+  );
+}
