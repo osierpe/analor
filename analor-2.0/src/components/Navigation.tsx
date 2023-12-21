5e60ac26bef6a0fae5299a24b80cbc6c44681df2
Luiz T. <90431370+LuizHTdS@users.noreply.github.com>
2023-12-21 12:19:08 -0300

    refatorado navbar

--- analor-2.0/src/components/Navigation.tsx
+++ analor-2.0/src/components/Navigation.tsx
@@ -23,13 +23,21 @@ export default function Navigation({
 
   const navigation_elements = navigation_names.map((nome, i) => {
     return (
-      <div
-        className={`nav_link ${cur_page === i ? 'active' : ''}`}
-        key={nome}
-        onClick={() => set_cur_page(i)}
-      >
-        <h2>{formatar_underlines(nome)}</h2>
-      </div>
+      <>
+        <div
+          className={`nav_link ${cur_page === i ? 'active' : ''}`}
+          key={nome}
+          onClick={() => set_cur_page(i)}
+        >
+          <h2>{formatar_underlines(nome)}</h2>
+        </div>
+
+        {i === navigation_names.length - 1 ? (
+          ''
+        ) : (
+          <div className="separator"></div>
+        )}
+      </>
     )
   })
 
