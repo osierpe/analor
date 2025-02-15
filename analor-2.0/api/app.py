bf6a8d45dd190430c2052cbbcfdfaaee9de4c77c
Feykro 🐝 <f3ykr0@gmail.com>
2025-02-15 19:38:33 -0300

    bug fix

--- analor-2.0/api/app.py
+++ analor-2.0/api/app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 @app.route('/abrev')
 def getAbrev():
     args = request.args
-    conn = psycopg2.connect(database="new_db_analor",
+    conn = psycopg2.connect(database="analor",
                             user="postgres",
-                            password="admin",
+                            password="feykro",
                             host="localhost", port="5432")
   
 
@@ -30,9 +30,9 @@ def search():
     args = request.args
     parameter_dict = args.to_dict()['data']
     parameter_dict = json.loads(parameter_dict)
-    conn = psycopg2.connect(database="new_db_analor",
+    conn = psycopg2.connect(database="analor",
                             user="postgres",
-                            password="admin",
+                            password="feykro",
                             host="localhost", port="5432")
   
 
@@ -54,9 +54,9 @@ def search():
         firstClause = False
 
     if whereclause:
-        cur.execute(f'''SELECT * FROM univ1_210523 WHERE {whereclause};''')
+        cur.execute(f'''SELECT * FROM univ1_050924 WHERE {whereclause};''')
     else:
-        cur.execute(f'''SELECT * FROM univ1_210523 ;''')
+        cur.execute(f'''SELECT * FROM univ1_050924 ;''')
 
     resultRows = cur.fetchall()
   
--- analor-2.0/src/components/Elementos.tsx
+++ analor-2.0/src/components/Elementos.tsx
@@ -14,7 +14,7 @@ export default function Elementos({
       if (el.nome !== nome) return el
       return {
         nome: el.nome,
-        quantidade: val ? el.quantidade : null,
+        quantidade: val ? el.quantidade : '',
         tem: val,
       }
     })
